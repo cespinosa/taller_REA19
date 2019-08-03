3ef3a650eaacc984b5ff752d3c137fc30ace30ab
Carlos Espinosa <espinosa@miztli>
2019-08-02 22:02:30 -0500

    first commit

---
+++ codes/1.py
@@ -0,0 +1,17 @@
+#Constantes
+g = 9.81
+
+#Condiciones iniciales
+x0 = 0
+y0 = 0
+vy0 = 10
+vx0 = 6
+
+t=10
+
+#Calculando posicion
+x = vx0 * t + x0
+y = y0 + vy0 * t - 0.5 * g * t ** 2
+
+print('La posicion de la particula es:')
+print('x={}\ny={}'.format(x,y))
